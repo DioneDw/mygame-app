22a044cbf0ac2efd3aa2003fd53eec6813127991
DioneDw <dionewilly@hotmail.com>
2021-12-04 13:58:25 -0300

    master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Link, Route} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 function App() {
+
+  const cart = useSelector(state => state.cart);
+  const {cartItems} = cart;
+
   return (
   <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">Mygame</a>
+          <Link className="brand" to="/">MeuJogo</Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+          <Link to="/cart">Carrinho</Link>
+          {cartItems.length > 0 && (
+            <span className="badge">{cartItems.length}</span>
+          )}
+          <Link to="/signin">| Faça seu Login!</Link>
         </div>
       </header>
       <main>
@@ -21,7 +29,9 @@ function App() {
         <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
        </main>
-      <footer className="row center">Todos os direitos reservados Dione</footer>
+      <footer className="row center">
+        Todos os direitos reservados, Projeto desenvolvido para trabalho Banco de Dados 2- UFOP
+      </footer>
     </div>
   </BrowserRouter>
   );

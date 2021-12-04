bcb070eb3d9ef6a3f5d444abc943836c13f29933
DioneDw <dionewilly@hotmail.com>
2021-12-04 18:08:01 -0300

    master

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,26 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Dione',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Consumidor1',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+    {
+        name: 'Consumidor2',
+        email: 'user2@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false,
+      },
+  ],
     products: [
         {
             _id: '1',

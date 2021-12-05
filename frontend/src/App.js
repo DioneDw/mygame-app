9c9b7abc2bc1d14ed4de1dcc85b2dbf338dd880d
DioneDw <dionewilly@hotmail.com>
2021-12-05 14:16:58 -0300

    master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,64 @@
 import React from 'react';
-import { BrowserRouter, Link, Route} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import {signout}  from './actions/userActions';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
 
-  const cart = useSelector(state => state.cart);
-  const {cartItems} = cart;
-
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+  
   return (
-  <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">MeuJogo</Link>
-        </div>
-        <div>
-          <Link to="/cart">Carrinho</Link>
-          {cartItems.length > 0 && (
-            <span className="badge">{cartItems.length}</span>
-          )}
-          <Link to="/signin">| Faça seu Login!</Link>
-        </div>
-      </header>
-      <main>
-        <Route path="/cart/:id?" component={CartScreen}></Route>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/signin" component={SigninScreen}></Route>
-        <Route path="/" component={HomeScreen} exact></Route>
-       </main>
-      <footer className="row center">
-        Todos os direitos reservados, Projeto desenvolvido para trabalho Banco de Dados 2- UFOP
-      </footer>
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Meujogo
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Carrinho
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Deslogar
+                    </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">| Faça seu Login!</Link>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">Todos os direitos reservados, Projeto desenvolvido para trabalho Banco de Dados 2- UFOP</footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,54 +1,72 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen() {
+export default function SigninScreen(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const submitHandler = (e) => {
-        e.preventDefault();
-        // TO DO SIGN IN ACTION
+  const redirect = props.location.search
+    ? props.location.search.split('=')[1]
+    : '/';
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(signin(email, password));
+  };
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
     }
-
-    return (
+  }, [props.history, redirect, userInfo]);
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
         <div>
-            <form className="form" onSubmit={submitHandler}>
-                <div>
-                    <h1>Faça seu Login</h1>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        placeholder="Enter email" 
-                        required
-                        onChange={e => setEmail(e.target.value)}>
-                    </input>
-                </div>
-                <div>
-                    <label htmlFor="password">Senha</label>
-                    <input 
-                        type="password" 
-                        id="password" 
-                        placeholder="Enter password" 
-                        required
-                        onChange={e => setPassword(e.target.value)}>
-                    </input>
-                </div>
-                <div>
-                    <label/>
-                    <button className="primary" type="submit">Logar</button>
-                </div>
-                <div>
-                    <label/>
-                    <div>
-                        Novo usuário? {' '}
-                        <Link to="/register">Registre-se</Link>
-                    </div>
-                </div>
-            </form>
-
+          <h1>Efetue seu Login</h1>
         </div>
-    );
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Fazer Login!
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            Novo Usuário? <Link to="/register">Cria sua Conta</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
